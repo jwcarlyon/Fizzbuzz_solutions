8f2c88b1a0d5900369e981c637106b7a89218382
yusuf <jwcarlyon@yahoo.com>
2020-09-16 12:07:29 -0500

    adding functional c++ solution to fizzbuzz

--- fizzbuzz/c++/fizzbuzz.cpp
+++ fizzbuzz/c++/fizzbuzz.cpp
@@ -5,33 +5,41 @@ For each multiple of 3, print "Fizz" instead of the number.
 For each multiple of 5, print "Buzz" instead of the number.
 
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.*/
-
+#include <stdio.h>
 int main()
 {
   int num = 1;
-  while(num < 100)
+  while(num < 101)
   {
-    char str[8];
-    if(num % 3)
+    char str[8] = {'\0'};
+    if(num % 3 == 0)
     {
       str[0] = 'F'; str[1] = 'i'; str[2] = 'z'; str[3] = 'z';
-      if(num % 5)
+      if(num % 5 == 0)
       {
         str[4] = 'B'; str[5] = 'u'; str[6] = 'z'; str[7] = 'z';
       }
     }
-    else if(num % 5)
+    else if(num % 5 == 0)
     {
       str[0] = 'B'; str[1] = 'u'; str[2] = 'z'; str[3] = 'z';
     }
-    if(str[0]):
+    if(str[0] != '\0')
     {
-      printf(str);
+      //for(int i = 0; i < strlen(str); i++)
+      int i = 0;
+      while(str[i] != '\0')
+      {
+        printf("%c", str[i]);
+        i++;
+      }
     }
     else
     {
-      printf(num);
+      printf("%i", num);
     }
+    printf("\n");
+    num++;
   }
   return 0;
 }
