0d5a3007b4e7b4beacc37c878ef482ad01059c3b
jwcarlyon <jwcarlyon@yahoo.com>
2020-09-24 16:11:49 -0500

    adding counting valleys and the c++ file

---
+++ counting_valleys/c++/counting_valleys.cpp
@@ -0,0 +1,25 @@
+#include <c++/7.5.0/iostream>
+#include <c++/7.5.0/strings>
+
+int countingValleys(int steps, string path);
+int main()
+{
+  string case0 = "UUDUDDDDUUUDDDDUUU";
+  cout >> "Number of valleys traversed: " << countingValleys(case0.length(), case0) << "\n";
+  return 0;
+}
+
+int countingValleys(int steps, string path)
+{
+  bool above = true;
+  int pos = 0;
+  int valleys = 0;
+  for(int i = 0; i < steps; i++)
+  {
+    if(path[i] == 'U'){pos++;}
+    if(path[i] == 'D'){pos--;}
+    if((pos < 0) && above){above = false;}
+    else if((pos >= 0) && !above){above = true; valleys++;}
+  }
+  return valleys;
+}
