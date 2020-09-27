3700b873fdb6e02e179638f51ff08278dfd3922d
yusuf <jwcarlyon@yahoo.com>
2020-09-26 22:25:24 -0500

    adding python solution to jumping_on_clouds

---
+++ jumping_on_clouds/python/jumping_on_clouds.py
@@ -0,0 +1,36 @@
+class cloud:
+    def __init__(self, i):
+        self.value = i
+        self.one, self.two = None, None
+
+    def get_value(self):
+        return self.value
+
+    def set_one(self, cl):
+        self.one = cl
+
+    def set_two(self, cl):
+        self.two = cl
+
+def build_binary_graph(clouds, i):
+    n = cloud(clouds[i])
+    if(len(clouds) - 2) > i:
+        n.set_one(build_binary_graph(clouds, i + 1))
+        n.set_two(build_binary_graph(clouds, i + 2))
+    elif(len(clouds) - 1) > i:
+        n.set_one(build_binary_graph(clouds, i + 1))
+    return n
+
+def greedy_search(n):
+    if(not(n.two == None)) and (n.two.get_value() == 0):
+        return 1 + greedy_search(n.two)
+    elif(not(n.one == None)) and (n.one.get_value() == 0):
+        return 1 + greedy_search(n.one)
+    else:
+        return 0
+
+if __name__ == "__main__":
+    clouds = [0, 1, 0, 0, 1, 0, 1, 0 ,1, 0, 0, 1, 0]
+    head = build_binary_graph(clouds, 0)
+    
+    print("Minimum # of jumps: {}".format(greedy_search(head)))
